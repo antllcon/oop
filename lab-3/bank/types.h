a84af57baadc00e8cf1a41ec57d9fdb9c9b5d9fa
antllcon <pokemonivan32@gmail.com>
2025-05-16 20:18:35 +0300

    Добавлен enum class для хранения имен

--- lab-3/bank/types.h
+++ lab-3/bank/types.h
@@ -3,5 +3,17 @@
 
 using AccountId = unsigned long long;
 using Money = long long;
+enum class Name
+{
+	Homer,
+	Marge,
+	Lisa,
+	Bart,
+	Apu,
+	Berns,
+	Nelson,
+	Chester,
+	Waylon
+};
 
 #endif // TYPES_H
